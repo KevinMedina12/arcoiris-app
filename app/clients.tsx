30977025c91c1dcd21494afae43ad336fe26fba1
KevinMedina12 <kevinmedina3003@gmail.com>
2024-03-20 20:51:28 -0600

    cambios-aqui

--- app/clients.tsx
+++ app/clients.tsx
@@ -7,7 +7,7 @@ import { useLanguage } from "./context/LanguageProvider";
 const menuOptions = [
   {
     icon: require("../assets/images/prepa15.png"),
-    label: "Preparatoria 15345345",
+    label: "Preparatoria 233333333333",
     href: "clients",
   },
   {
